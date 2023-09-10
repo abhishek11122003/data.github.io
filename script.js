66663cb1acc79d7e52f42ada36875073143ec268
Abhishek <84235903+abhishek11122003@users.noreply.github.com>
2023-09-10 15:31:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-function calculate(){
-   let deposit = document.querySelector("#deposit").value;
-
-   let com = parseFloat(deposit)*0.05;
-   let tax = parseFloat(com)*0.5;
-   let final = parseFloat(com) - parseFloat(tax);
-
-   document.querySelector(".com").innerHTML = com;
-   document.querySelector(".tax").innerHTML = tax;
-   document.querySelector(".final").innerHTML = final;
+function calculate() {
+    let deposit = parseFloat(document.querySelector("#deposit").value);
+ 
+    // Calculate the commission as 0.5% of the deposit
+    let com = deposit * 0.005; // 0.5% represented as a decimal
+    let tax = com * 0.05; // 5% tax on the commission
+    let final = com - tax; // Final amount after deducting tax
+ 
+    document.querySelector(".com").innerHTML = com.toFixed(2); // Display with 2 decimal places
+    document.querySelector(".tax").innerHTML = tax.toFixed(2); // Display with 2 decimal places
+    document.querySelector(".final").innerHTML = final.toFixed(2); // Display with 2 decimal places
 }
